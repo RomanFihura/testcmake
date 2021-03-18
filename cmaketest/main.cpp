92895d7cd72af23cde5a85272431a5674ff3b10d
Roman Fihura <romanfigura120@gmail.com>
2021-03-18 19:37:53 +0200

    newupdate

---
+++ cmaketest/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    cout<<"Hello world of VSCODE2";
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    cout<<"Hello world of VSCODE3";
+    return 0;
+}
